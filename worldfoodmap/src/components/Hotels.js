63354cd24783ec78a298a892f0133e65b6a0cf8e
nkalkstein <nkalkstein@gmail.com>
2018-08-18 10:20:04 -0400

    added Hotels component

---
+++ worldfoodmap/src/components/Hotels.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import MainPage from './MainPage';
+
+
+Hotels Component:
+
+Use hotel metasearch site api.  Display real time results on video page?
+
+
+
+
+return(
+	<div onClick={() => Flight(time)}>
+		<div>
+			<div style={{float: "left", width: "24%", marginRight: "1%", marginBottom: "0.5em", color: "white"}}>
+				<img  alt="alt" src={imageUrl} />
+				<h4>{flightInfo}</h4>
+			</div>
+			<div>
+				
+			</div>
+		</div>
+	</div>
+
+
+
+	)
+}
+
+
+
+
+
+export default Hotels
