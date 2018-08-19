755b5d196e719e654b073a894e565ac416c05b54
nkalkstein <nkalkstein@gmail.com>
2018-08-19 08:49:10 -0400

    worked on hotel component

--- worldfoodmap/src/components/Flights.js
+++ worldfoodmap/src/components/Flights.js
@@ -11,10 +11,7 @@ const Flight = ({time, date, airline, flightNumber}) => {
 return(
 	<div onClick={() => Flight(time)}>
 		<div>
-			<div style={{float: "left", width: "24%", marginRight: "1%", marginBottom: "0.5em", color: "white"}}>
-				<img  alt="alt" src={imageUrl} />
-				<h4>{flightInfo}</h4>
-			</div>
+
 			<div>
 				
 			</div>
