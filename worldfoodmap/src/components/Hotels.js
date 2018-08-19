9c61f750b52460ab79299c7cc79d1cc3658766de
nkalkstein <nkalkstein@gmail.com>
2018-08-19 08:41:59 -0400

    worked on hotel component

--- worldfoodmap/src/components/Hotels.js
+++ worldfoodmap/src/components/Hotels.js
@@ -12,9 +12,6 @@ Use hotel metasearch site api.  Display real time results on video page?
 return(
 	<div onClick={() => Flight(time)}>
 		<div>
-			<div style={{float: "left", width: "24%", marginRight: "1%", marginBottom: "0.5em", color: "white"}}>
-				<img  alt="alt" src={imageUrl} />
-				<h4>{flightInfo}</h4>
 			</div>
 			<div>
 				
