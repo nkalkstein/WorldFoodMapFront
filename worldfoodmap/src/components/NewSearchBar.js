934817c06be9e5c0e6c0f4744e7c128b6135187b
nkalkstein <nkalkstein@gmail.com>
2019-01-28 22:39:20 -0500

    worked on searchbar component

--- worldfoodmap/src/components/NewSearchBar.js
+++ worldfoodmap/src/components/NewSearchBar.js
@@ -2,6 +2,7 @@ import React from "react"
 import Autosuggest from 'react-autosuggest';
 
 // autosuggest new place? add to map?
+// Go straight to flights? Hotels? from search bar?
 // Imagine you have a list of languages that you'd like to autosuggest.
 const cities = [
         { name: "Amsterdam", country:"Holland", coordinates: [4.54, 52.22] },
